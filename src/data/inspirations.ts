c4738627e559133f0bf9e728749b043b59674021
Sovann DUFRESNE <sovann.dufresne@epitech.eu>
2025-10-18 19:21:02 +0200

    Added the first batch of inspiration cards

---
+++ src/data/inspirations.ts
@@ -0,0 +1,30 @@
+export const inspirations = [
+  {
+    title: "Octopath Traveler",
+    desc: "Un RPG au style artistique unique mélangeant pixel art et graphismes en haute définition, avec une bande-son immersive.",
+    image: "/images/octopath-traveler.jpg",
+    tags: ["Videogame", "Visuel", "Ambiance"],
+    href: "https://store.steampowered.com/app/921570/OCTOPATH_TRAVELER/",
+    fullsize: false
+  },
+  {
+    title: "Vaporwave aesthetics",
+    image: "/images/vaporwave.png",
+    tags: ["Vaporwave", "Ambiance", "Couleurs"],
+    fullsize: true
+  },
+  {
+    title: "Minecraft",
+    desc: "Un jeu bac à sable qui permet une créativité illimitée avec ses graphismes en blocs et son monde ouvert.",
+    image: "/images/minecraft.png",
+    tags: ["Videogame", "Créativité", "Pixel Art"],
+    href: "https://www.minecraft.net/",
+    fullsize: false
+  },
+  {
+    title: "Cyberpunk aesthetics",
+    image: "/images/cyberpunk.jpg",
+    tags: ["Cyberpunk", "Vibe", "Couleurs"],
+    fullsize: true
+  },
+];
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,7 @@ export const featured = [
   {
     title: "The Greed",
     image: "/images/the-greed-cover.jpg",
-    desc: "RPG 2.5D avec pixel-art, vibes néon, mécaniques de “greedy thief”.",
+    desc: "RPG 2.5D avec pixel-art, vibes gore, mécaniques de “greedy thief”.",
     tags: ["Game Dev", "Unity", "Pixel Art"],
     href: "https://github.com/ton-repo/the-greed"
   }
